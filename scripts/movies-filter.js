493067df77204d7c86475029c0568eb6a730da34
Alejandro Anibal Isa <alejandroisa71@gmail.com>
2023-06-17 08:47:02 -0300

    Iniatial destructuration filmCritic

--- scripts/movies-filter.js
+++ scripts/movies-filter.js
@@ -1,5 +1,24 @@
-import movies from "../data/movies.js";
-import users from "../data/users.js";
+import movies from '../data/movies.js';
+import users from '../data/users.js';
 
-console.log(movies)
-console.log(users)
+///esto traera del input
+const user = users[0];
+const { id, username, email, address, company:nameCompany} = user;
+const { city, street } = address;
+const { name:company } = nameCompany;
+
+const filmCritic = class {
+  constructor(id, username, email, street, city, company ) { // ojo con nombre company
+    this.id = id;
+    this.username = username;
+    this.email = email;
+    this.fullAddress=`${street} - ${city}`;
+    this.company = company;
+    //this.movie= movie.title;-------aca iran arreglos de peliculas
+    //this.rate=movie.rate;
+  }
+};
+
+const critico = new filmCritic(id, username, email, street, city, company); //no pasar tantos parametros
+console.log(critico.company);
+
