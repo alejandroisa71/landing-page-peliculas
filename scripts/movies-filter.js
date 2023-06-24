ab89c732fad31a3749e8017de521bffc73918e91
Alejandro Anibal Isa <alejandroisa71@gmail.com>
2023-06-24 00:42:03 -0300

    Aggregatin image for search and teminatilly styles

--- scripts/movies-filter.js
+++ scripts/movies-filter.js
@@ -2,88 +2,131 @@ import movies from '../data/movies.js';
 import users from '../data/users.js';
 
 const rate = document.getElementById('rate');
-const films = document.querySelector('ul[name="films"]');
-const resultCode = document.getElementById('result');
-const fromDate = document.getElementById('fromDate'); //validar cuando ingrese una año menor
-const toDate = document.getElementById('toDate'); //validar cuando ingrese una año menor
+const listMovies = document.querySelector('ul[name="listMovies"]');
+const fromDate = document.getElementById('fromDate');
+const toDate = document.getElementById('toDate');
 const btnMostrar = document.getElementById('btnMostrar');
-
-//---------------------------------------------------------
 const $select = document.querySelector('#select');
 
-const agregar = () => {
+//---------------LLena Select con los Críticos------------
+
+const fillSelectCritics = () => {
   users.forEach((user) => {
     const option = document.createElement('option');
-    const valor = user.username;
+    const value = user.username;
     option.value = user.id;
-    option.text = valor;
+    option.text = value;
     $select.appendChild(option);
-    // console.log(option.value);
   });
 };
-agregar();
+fillSelectCritics();
 
 const mostrar = () => {
-  const indice = $select.selectedIndex;
-  if (indice === -1) return; // Esto es cuando no hay elementos
-  const opcionSeleccionada = $select.options[indice];
-  // console.log(opcionSeleccionada.value);
-  return opcionSeleccionada.value;
+  const index = $select.selectedIndex;
+  // if (index === -1) return; // Esto es cuando no hay elementos
+  const selectOption = $select.options[index];
+  return selectOption.value;
 };
 
-// document.addEventListener('DOMContentLoaded', () => {
-// });
-// const userId=mostrar(users)
-// console.log(userId);
-
-// const userId= mostrar()
-// console.log(userId);
-
-//----------------------------------------------
-
-// console.log(option1);
-
 const FILMS_FUNCTION = {
-  pintarMovies: function (movies ,users) {
-    while (films.firstChild) {
-      films.removeChild(films.firstChild);
-    }
-    
-    return users.filter((_, index)) => movies[index] = movies.userId///continuara
+  showMovies: function (movies, users) {
+    const listMoviesFilter = [];
 
-    const filmCritic = class {
-      constructor(id, username, email, street, city, company,  title , rate) {
-        this.id = id;
-        this.username = username;
-        this.email = email;
-        this.fullAddress = `${street} - ${city}`;
-        this.company = company;
-        this.title = title;
-        this.rate = rate
+    while (listMovies.firstChild) {
+      listMovies.removeChild(listMovies.firstChild);
+    }
+
+    if(movies.length === 0){
+        console.log('no hay lementos');
+        let option = document.getElementsByTagName('li')[0];
+      option = document.createElement('li');
+      option.class = 'errorSearch';
+      // option.id = `${movie.id}`;
+      listMovies.appendChild(option);
+      const title = document.createElement('h3');
+      title.textContent = "No hay Resultados para la busqueda";
+      option.appendChild(title);
+      const image = document.createElement('img');
+      image.src = "./img/errorBusqueda.jpg";
+      image.width = 600;
+      image.height=400
+      option.appendChild(image);
+
+        return
+    }
+
+    const movieCritic = class {
+      constructor(user, movie) {
+        this.id = user.id;
+        this.username = user.username;
+        this.email = user.email;
+        this.fullAddress = `${user.address.street} - ${user.address.city}`;
+        this.company = user.company.name;
+        this.title = movie.title;
+        this.rate = movie.rate;
+        this.image = movie.image;
       }
     };
 
-
     movies.forEach((movie) => {
+      const { userId } = movie;
+      users.forEach((user) => {
+        const { id } = user;
+
+        if (id === userId) {
+          const objMovie = new movieCritic(user, movie);
+          listMoviesFilter.push(objMovie);
+        }
+      });
+    });
+
+   
+
+    listMoviesFilter.forEach((movie) => {
       let option = document.getElementsByTagName('li')[0];
       option = document.createElement('li');
-      option.className = 'option';
+      option.class = 'option';
       option.id = `${movie.id}`;
-      option.textContent = `${movie.title} - ${movie.rate} - ${movie.userId}`;
-      films.appendChild(option);
+      listMovies.appendChild(option);
+
+      const title = document.createElement('h3');
+      title.textContent = `${movie.title}`;
+      option.appendChild(title);
+
+      const image = document.createElement('img');
+      image.src = `${movie.image}`;
+      image.width = 200;
+      option.appendChild(image);
+
+      let rate = document.createElement('rate');
+      rate.textContent = `Puntuación: ${movie.rate}`;
+      rate.class = 'rate';
+      option.appendChild(rate);
+
+      const username = document.createElement('h3');
+      username.textContent = `${movie.username}`;
+      option.appendChild(username);
+      const email = document.createElement('p');
+      email.textContent = `Email: ${movie.email}`;
+      option.appendChild(email);
+      const fullAddress = document.createElement('p');
+      fullAddress.textContent = `Dirección: ${movie.fullAddress}`;
+      option.appendChild(fullAddress);
+      const company = document.createElement('p');
+      company.textContent = `Compañia: ${movie.company}`;
+      option.appendChild(company);
     });
   },
 
   // Hace los filtrados de peliculas
-  filterMovies: function (users, movies, userId, rate, fromDate, toDate) {
-    console.log(typeof userId);
-
+  filterMovies: function (_users, movies, userId, rate, fromDate, toDate) {
     const moviesCloned = movies.map((movie) => {
       return {
         title: movie.title,
         rate: movie.rate,
         watched: movie.watched,
         userId: movie.userId,
+        image: movie.image,
       };
     });
 
@@ -108,16 +151,15 @@ const FILMS_FUNCTION = {
   },
 };
 
-const defaultOption = 'pintarMovies';
+const defaultOption = 'showMovies';
 const choosenFunction =
   FILMS_FUNCTION[defaultOption] ?? FILMS_FUNCTION.defaultOption;
-choosenFunction(movies);
-
+choosenFunction(movies, users);
 
 //Muestra todo lo filtrado
-function otraFuncion() {
+function otraFuncion() { 
   let option = 'filterMovies';
-  const userId = mostrar();
+  const userId = mostrar();// cambiar nombre a funcion
 
   let choosenFunction = FILMS_FUNCTION[option] ?? FILMS_FUNCTION.defaultOption;
   choosenFunction(
@@ -128,6 +170,7 @@ function otraFuncion() {
     fromDate.value,
     toDate.value
   );
+
   const filteredMovies = choosenFunction(
     users,
     movies,
@@ -136,37 +179,21 @@ function otraFuncion() {
     fromDate.value,
     toDate.value
   );
-  option = 'pintarMovies';
+
+  option = 'showMovies';
   choosenFunction = FILMS_FUNCTION[option] ?? FILMS_FUNCTION.defaultOption;
-  choosenFunction(filteredMovies);
+
+  choosenFunction(filteredMovies, users);
 }
+///----------------
+rate.addEventListener('change',otraFuncion);
 
-rate.addEventListener('change', () => {
-  otraFuncion();
-});
+fromDate.addEventListener('change', otraFuncion);
 
-fromDate.addEventListener('change', () => {
-  otraFuncion();
-});
-
-toDate.addEventListener('change', () => {
-  otraFuncion();
-});
+toDate.addEventListener('change', 
+  otraFuncion);
 
 btnMostrar.addEventListener('click', () => {
   mostrar;
   otraFuncion();
 });
-
-
-
-// const pepito = new filmCritic(1, 'ale', 'dsjfkl@gamil.com', 'san martin 999', 'tucuman', 'serra')
-
-
-// console.log(pepito);
-
-// filmCritic.prototype.rate =null
-// pepito.rate= 7.5
-// console.log(pepito);
-
-//no pasar tantos parametros
